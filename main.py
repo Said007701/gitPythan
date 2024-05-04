f48de85b12366babd39386de997b9b7900042a79
Пользователь <saidzalolovsaidaziz@gmail.com>
2024-05-04 19:41:41 +0500

    work

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-print("said")
-a=10
-print(a)
+# tst=5
+# if tst<10:
+#     print("<10")
+# else:
+#     print(">10")
+
+# tst=15
+# if tst<10:
+#     print(">10")
+# elif tst>20:
+#     print("<20")
+# else:
+#     print("<10")
+
+# tst=-5
+# if tst<0:
+#     print("<0")
+# elif tst<-10:
+#     print("<-10")
+# else:
+#     print("")
